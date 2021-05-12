d3a599f0244cc3021bc7cbddcbf1f7718b4c0d2b
Hamza <mr.humzza@gmail.com>
2021-05-12 02:09:33 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
